c1155d30ab404ac6332b8bae1fef466025895fdd
torenunez <salvador.nunez@berkeley.edu>
2019-08-07 19:40:59 +0200

    Section 7 Complete

--- 7-ES6/starter/script.js
+++ 7-ES6/starter/script.js
@@ -47,6 +47,10 @@ class Park extends Element {
     treeDensity() {
         return this.trees/this.area;
     }
+    
+    printTreeDensity(){
+        console.log(`${this.name} has a tree density of ${this.treeDensity()} trees per square km.`);
+    }
 }
 
 
@@ -59,72 +63,62 @@ class Street extends Element {
 }
 
 
-function avgAge(Parks) {
-    let totalAge = 0;
-    Parks.forEach(obj => totalAge += obj.calculateAge());
-    return totalAge/Parks.size
-}
+function calcTotAvg(arr) {
+    let total = 0;
+    
+    for (const a of arr) {
+        total += a
+       }
 
-function lengthInfo(Streets) {
-    let totalLength = 0;
-    Streets.forEach(obj => totalLength += obj.length);
-    return {
-        total: totalLength,
-        avg: totalLength/Streets.size
-    }
+    return [total, total/ arr.length]
 }
-
+    
 
 const Park1 = new Park('Parque Tezozomoc', 1900, 1512, 2151);
 const Park2 = new Park('Parc Guell', 1950, 15, 121);
 const Park3 = new Park('Griffith Park', 1960, 120, 1025);
+const parks = [Park1, Park2, Park3];
 
 const Street1 = new Street('Fair Oaks', 1915, 112);
 const Street2 = new Street('Reforma', 1890, 950, 'huge');
 const Street3 = new Street('Passeig Gracia', 1800, 50, 'tiny');
 const Street4 = new Street('Lopez Mateos', 1920, 512, 'big');
+const streets = [Street1, Street2, Street3, Street4];
 
 
+function reportParks(p) {
+    
+    console.log('---------------------------')
+    console.log('   Park Report')
+    console.log('---------------------------')
+    
+    // 1 
+    p.forEach(el => el.printTreeDensity()); 
+    
+    // 2
+    const [totalAge, avgAge] = calcTotAvg(p.map(el => el.calculateAge()));
+    console.log(`The average age for all parks is  ${avgAge}.`);
+    
+    // 3
+    const i = p.map(el => el.trees).findIndex(el => el >= 1000);
+    console.log(`${p[i].name} has ${p[i].trees} tres, which is greater than 1000.`)
 
-const Parks = new Map();
-Parks.set(1, Park1);
-Parks.set(2, Park2);
-Parks.set(3, Park3);
-
-
-const Streets = new Map();
-Streets.set(1, Street1);
-Streets.set(2, Street2);
-Streets.set(3, Street3);
-Streets.set(4, Street4);
-
-// Basic
-//Parks.forEach(obj => console.log(obj));
-//Streets.forEach(obj => console.log(obj));
-
-// Questions
-console.log('---------------------------')
-console.log('Elements Report')
-console.log('---------------------------')
-
-// 1 ...k, v order reversed in Python?
-Parks.forEach((value, key) => console.log(`Park ${key} is called ${value.name} and has a tree density of ${value.treeDensity()}.`)); 
-// 2
-console.log(`The average age for all parks is  ${avgAge(Parks)}.`)
-// 3
-Parks.forEach((value, key) => {
-    if (value.trees > 1000){
-        //console.log(key, value.name, value.trees)
-        console.log(`Park ${key} is called ${value.name} and has ${value.trees} tres, which is greater than 1000.`)
-    }
-})
-// 4
-const {total, avg} = lengthInfo(Streets);
-console.log(`The total length is ${total} and the avg length is ${avg}.`)
-// 5
-Streets.forEach((value, key) => console.log(`Street ${key} is called ${value.name} and is ${value.size}.`)); 
+};
 
 
+function reportStreets(s) {
+    
+    console.log('---------------------------')
+    console.log('   Streets Report')
+    console.log('---------------------------') 
+    
+    // 4
+    const [totalLength, avgLength] = calcTotAvg(s.map(el => el.length));
+    console.log(`The total street length is ${totalLength} and the average street length is ${avgLength}.`)
+    // 5
+    s.forEach(obj => console.log(`${obj.name} is ${obj.size}.`)); 
+};
 
 
-
+reportParks(parks);
+reportStreets(streets);
