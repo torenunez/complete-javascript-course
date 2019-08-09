49e416c684c1986773c77f7eec66deaec7e0c226
torenunez <salvador.nunez@berkeley.edu>
2019-08-09 22:45:06 +0200

    Complete up to 138

--- 9-forkify/starter/src/js/index.js
+++ 9-forkify/starter/src/js/index.js
@@ -1,4 +1,6 @@
 // Global app controller
-import x from './test';
+import string from './models/Search';
+import {add as a, multiply as m, ID} from './views/searchView';
+// import * as sv from './views/searchView';
 
-console.log(`I imported ${x} from another module`);
+console.log(`Using imported functions! Adding and multiplying 2 and ${ID}, is ${a(ID,2)} and ${m(ID,2)}, respectively. By the way, ${string}`)
