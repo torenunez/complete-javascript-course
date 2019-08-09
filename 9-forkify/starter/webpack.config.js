db58932c8f3e7d954e68f6551b14f1ab2c89ae94
torenunez <salvador.nunez@berkeley.edu>
2019-08-09 17:54:07 +0200

    up to 137

--- 9-forkify/starter/webpack.config.js
+++ 9-forkify/starter/webpack.config.js
@@ -2,7 +2,8 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/js/index.js',
+    entry: ['babel-polyfill', './src/js/index.js'],
+
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'js/bundle.js'
