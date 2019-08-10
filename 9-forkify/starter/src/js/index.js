03933d7231120b7d7aa2233f9005eb85cb7cb583
torenunez <salvador.nunez@berkeley.edu>
2019-08-10 22:57:06 +0200

    completed up to 140

--- 9-forkify/starter/src/js/index.js
+++ 9-forkify/starter/src/js/index.js
@@ -1,16 +1,5 @@
-// Global app controller
-import axios from 'axios';
+import Search from './models/Search';
 
-async function getResults(query){
-    const proxy = 'https://cors-anywhere.herokuapp.com/';
-    const key = '';
-    try{
-        const res = await axios(`${proxy}https://www.food2fork.com/api/search?key=${key}&q=${query}`);
-        const recipes = res.data.recipes;
-        console.log(recipes)
-    } catch (error){
-        alert(error);
-    }  
-};
+const search = new Search('chayote');
 
-getResults('chayote');
+search.getResults()
--- 9-forkify/starter/src/js/models/Search.js
+++ 9-forkify/starter/src/js/models/Search.js
@@ -1,1 +1,20 @@
-export default 'I am an exported string';
+import axios from 'axios';
+
+export default class Search {
+    constructor(query) {
+        this.query = query;
+    }
+
+    async getResults() {
+        const proxy = 'https://cors-anywhere.herokuapp.com/';
+        //https://www.food2fork.com/user/api#
+        const key = '';
+        try {
+            const res = await axios(`${proxy}http://food2fork.com/api/search?key=${key}&q=${this.query}`);
+            this.result = res.data.recipes;
+            console.log(this.result);
+        } catch (error) {
+            alert(error);
+        }
+    }
+}
