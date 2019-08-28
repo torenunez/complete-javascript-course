ed2ce5d7284d8794cec84a4bbfbdb643db518da5
Salvador Nunez <salvadornunez@fb.com>
2019-08-27 22:41:36 -0700

    141 complete

--- 9-forkify/starter/src/js/index.js
+++ 9-forkify/starter/src/js/index.js
@@ -1,5 +1,32 @@
 import Search from './models/Search';
 
-const search = new Search('chayote');
+/** Global state of the app
+ * - Search object
+ * - Current recipe object
+ * - Shopping list object
+ * - Liked recipes
+ */
+const state = {};
 
-search.getResults()
+const controlSearch = async () => {
+    // 1) Get query from view
+    const query = 'chayote' //TODO 
+
+    if (query){
+        // 2) New search obbject and add to state
+        state.search = new Search(query);
+
+        // 3) Prepare UI for results
+
+        // 4) Search for recipes
+        await state.search.getResults();
+
+        // 5) Render results on UI
+        console.log(state.search.result);
+    }
+}
+
+document.querySelector('.search').addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+});
--- 9-forkify/starter/src/js/models/Search.js
+++ 9-forkify/starter/src/js/models/Search.js
@@ -8,11 +8,11 @@ export default class Search {
     async getResults() {
         const proxy = 'https://cors-anywhere.herokuapp.com/';
         //https://www.food2fork.com/user/api#
-        const key = '';
+        const key = '3b014f72e90b4891bbfd60d9581e4529';
         try {
             const res = await axios(`${proxy}http://food2fork.com/api/search?key=${key}&q=${this.query}`);
             this.result = res.data.recipes;
-            console.log(this.result);
+            //console.log(this.result);
         } catch (error) {
             alert(error);
         }
