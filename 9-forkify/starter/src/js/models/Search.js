d34457d597ac8be389a8f68c6dc5242df4ac6adf
Salvador Nunez <salvadornunez@fb.com>
2019-08-28 20:58:42 -0700

    142 complete

--- 9-forkify/starter/src/js/models/Search.js
+++ 9-forkify/starter/src/js/models/Search.js
@@ -8,7 +8,7 @@ export default class Search {
     async getResults() {
         const proxy = 'https://cors-anywhere.herokuapp.com/';
         //https://www.food2fork.com/user/api#
-        const key = '3b014f72e90b4891bbfd60d9581e4529';
+        const key = '';
         try {
             const res = await axios(`${proxy}http://food2fork.com/api/search?key=${key}&q=${this.query}`);
             this.result = res.data.recipes;
